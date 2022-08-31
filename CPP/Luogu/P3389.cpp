ea01d9cae8f9cc53855d301e933442525e348100
Okimoto-TK <1303151482@qq.com>
2022-08-31 20:28:51 +0800

    .

--- CPP/Luogu/P3389.cpp
+++ CPP/Luogu/P3389.cpp
@@ -1,59 +1,84 @@
 #include <cstdio>
-#include <cmath>
 #include <algorithm>
+#define N 100
+const double eps = 1e-8;
 int n;
-double matrix[105][105];
-double x[105];
+double a[N + 5][N + 5];
+double x[N + 5];
+bool grt(int r, int x, int y)
+{
+	if(a[x][r] != a[y][r]) return a[x][r] > a[y][r];
+	for(int i = r + 1; i < n; i++)
+	{
+		if(a[x][i] != a[y][i]) return a[x][i] > a[y][i];
+	}
+	return false;
+}
+int gauss()
+{
+	int r, c;
+	for(r = 0, c = 0; r < n && c < n; r++, c++)
+	{
+		int max = r;
+		for(int j = r + 1; j < n; j++)
+		{
+			if(grt(c, j, max))
+			{
+				max = j;
+			}
+		}
+		if(max != r) std::swap(a[r], a[max]);
+		if(std::abs(a[r][c]) < eps)
+		{
+			r--;
+			continue;
+		}
+		for(int j = r + 1; j < n; j++)
+		{
+			if(std::abs(a[j][c]) < eps) continue;
+			double v = a[j][c] / a[r][c];
+			for(int k = c; k <= n; k++)
+			{
+				a[j][k] -= a[r][k] * v;
+			}
+			a[j][c] = 0;
+		}
+	}
+	for(int i = r; i < n; i++)
+	{
+		if(std::abs(a[i][n]) >= eps) return -1;
+	}
+	if(r < n) return n - r;
+	for(int i = n - 1; i >= 0; i--)
+	{
+		for(int j = i + 1; j < n; j++)
+		{
+			a[i][n] -= a[i][j] * x[j];
+		}
+		x[i] = a[i][n] / a[i][i];
+	}
+	return 0;
+}
 int main()
 {
 	scanf("%d", &n);
-	for(int i = 1; i <= n; i++)
+	for(int i = 0; i < n; i++)
 	{
-		for(int j = 1; j <= n + 1; j++)
+		for(int j = 0; j <= n; j++)
 		{
-			scanf("%lf", &matrix[i][j]);
+			scanf("%lf", &a[i][j]);
 		}
 	}
-	for(int i = 1; i <= n; i++)
+	if(gauss() != 0)
 	{
-		double max = -(0x3f3f3f3f);
-		int ptr = -1;
-		for(int j = i; j <= n; j++)
+		printf("No Solution");
+	}
+	else
+	{
+		for(int i = 0; i < n; i++)
 		{
-			if(max < matrix[j][i])
-			{
-				max = matrix[j][i];
-				ptr = j;
-			}
-		}
-		for(int j = 1; j <= n + 1; j++)
-		{
-			std::swap(matrix[ptr][j], matrix[i][j]);
-		}
-		for(int j = 1; j <= n; j++)
-		{
-			if(i == j) continue;
-			double r = matrix[j][i] / matrix[i][i];
-			for(int k = 1; k <= n + 1; k++)
-			{
-				matrix[j][k] -= matrix[i][k] * r;
-			}
+			printf("%.2lf\n", x[i]);
 		}
 	}
-	for(int i = n; i >= 1; i--)
-	{
-		double ans = matrix[i][n + 1];
-		for(int j = n; j > i; j--)
-		{
-			ans -= matrix[i][j] * x[j];
-		}
-		if(fabs(matrix[i][i]) <= 0.00000001)
-		{
-			printf("No Solution");
-			return 0;
-		}
-		x[i] = ans / matrix[i][i];
-	}
-	for(int i = 1; i <= n; i++) printf("%.2lf\n", x[i]);
 	return 0;
 }
